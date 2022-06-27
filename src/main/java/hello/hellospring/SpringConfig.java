e613e10719c7b352cbcf8fde82df810dfd1c5346
hesung <hsjo6864@gmail.com>
2022-06-27 23:47:16 +0900

    2022.06.27 Spring Practice - 스프링 데이터 JPA 내용 추가

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -15,6 +15,8 @@ public class SpringConfig {
 
     //@Autowired private DataSource dataSource;
 
+
+    /*
     //@PersistenceContext
     private final EntityManager em;
 
@@ -22,17 +24,25 @@ public class SpringConfig {
     public SpringConfig(EntityManager em) {
         this.em = em;
     }
+    */
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+//    @Bean
+//    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+//    }
 }
