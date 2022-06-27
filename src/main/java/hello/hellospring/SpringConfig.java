9f2fe43c53ac0ce643ef6c3ad73aa79e72706bff
hesung <hsjo6864@gmail.com>
2022-06-27 23:27:58 +0900

    2022.06.27 Spring Practice - 간단한 회원가입 및 조회 화면 예시 - H2 데이터베이스 스프링부트 연동 - 순수 JDBC, JDBC Template, JPA 사용한 repository 구현 예제

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,27 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    //@Autowired private DataSource dataSource;
+
+    //@PersistenceContext
+    private final EntityManager em;
+
+    @Autowired
+    public SpringConfig(EntityManager em) {
+        this.em = em;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +30,9 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
